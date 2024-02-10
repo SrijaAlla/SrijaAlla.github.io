a37ef51fbaaffe50f7ad3a44615bc6a108e45007
SrijaAlla <srijaalla10@gmail.com>
2024-02-10 00:54:36 -0500

    added in art page

--- src/Art.js
+++ src/Art.js
@@ -11,19 +11,27 @@ import image15 from './assets/15.JPG'
 import image16 from './assets/16.JPG'
 import image17 from './assets/Cat.jpeg'
 import image18 from './assets/clean.jpeg'
+import image19 from './assets/18.jpg'
+import image20 from './assets/19.jpg'
+import image21 from './assets/17.jpg'
+import image22 from './assets/20.jpeg'
+
 // import art from "./assets/artbg.jpg";
 const Art = () => {
   return (
     <div className="Art">
       <img src={image5} className="art-image"></img>
       <img src={image6} className="art-image"></img>
+      <img src={image22} className="art-image"></img>
+
       <img src={image9} className="art-image"></img>
       {/* <img src={image7} className="art-image"></img> */}
       <img src={image2} className="art-image"></img>
       <img src={image15} className="art-image"></img>
       <img src={image17} className="art-image"></img>
-      <img src={image16} className="art-image"></img>
+      <img src={image21} className="art-image"></img>
       <img src={image18} className="art-image"></img>
+      <img src={image20} className="art-image"></img>
 
       <img src={image8} className="art-image"></img>
       <img src={image1} className="art-image"></img>
